b8a52351bca5c388ac1bbb2a2da19a7189cbc2a2
nboy4 <nboy4521@gmail.com>
2023-07-13 00:07:35 +0700

    -

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import { array } from './array.js';
+import array from "./array.js";
 
-const array = require('./array.js');
-const button = document.querySelector('.content__button');
-const input = document.querySelector('.content__input');
+const button = document.querySelector(".content__button");
+const input = document.querySelector(".content__input");
 
 function randomClick() {
-    const random = array[(Math.floor(Math.random() * (array.length)))];
-    const random2 = array[(Math.floor(Math.random() * (array.length)))];
-    button.addEventListener('click', function() {
-        input.value = random + ' ' + random2;
-    })
+  const random = array[Math.floor(Math.random() * array.length)];
+  const random2 = array[Math.floor(Math.random() * array.length)];
+  input.value = random + " " + random2;
 }
 
-randomClick(array);
+button.addEventListener("click", randomClick);
+
+randomClick();
